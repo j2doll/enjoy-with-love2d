7bef9e94b9262bee67cc184a0a14679d8f744695
Jay Two <1289306+j2doll@users.noreply.github.com>
2018-12-18 13:21:52 +0900

    testing

--- 01.load-image-with-exception/main.lua
+++ 01.load-image-with-exception/main.lua
@@ -1,6 +1,8 @@
 -- main.lua
 
 local ham = nil
+local x_pos = 0
+local y_pos = 0
 
 function i_might_throw_exception()
 	if something_is_wrong then
@@ -9,7 +11,7 @@ function i_might_throw_exception()
 	ham = love.graphics.newImage("hamster.png") 
 	return something
 end
--- code from http://ruoyusun.com/2013/03/30/pragmatic-lua-error-handling-oop-closure-and-coroutines.html
+-- main code from http://ruoyusun.com/2013/03/30/pragmatic-lua-error-handling-oop-closure-and-coroutines.html
 
 function love.load()
     
@@ -17,14 +19,18 @@ function love.load()
     local success, result = pcall(i_might_throw_exception)
     if not success then
         print(result) -- print out error message
+        return
     end    
 
-    x = 50
-    y = 50
+    x_pos = 50
+    y_pos = 50
 end
   
 function love.draw()
     if ham ~= nil then 
-        love.graphics.draw(ham, x, y)
-    end 
+        love.graphics.draw(ham, x_pos, y_pos)
+    else
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.rectangle("fill", x_pos, y_pos, 100, 100) -- x_pos and y_pos is zero.   
+    end
 end
